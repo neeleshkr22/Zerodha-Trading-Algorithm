51813529eb57f626cd8d5be46b7ce30f063b3cd9
Neelesh <neeleshrana22@gmail.com>
2025-06-23 20:30:43 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,78 @@ export const app = express();
 
 app.use(bodyParser({}));
 
-interface Balances{
+interface Balances {
     [key: string]: number;
 }
 
-interface User{
+interface User {
     id: string;
-    balance: Balances;
+    balances: Balances;
 }
 
-interface Order{
+interface Order {
     userId: string;
     price: number;
-    quantity : number;
+    quantity: number;
 }
 
 export const TICKER = "GOOGLE";
 
+const users: User[] = [{
+    id: '1',
+    balances: {
+        "Google": 10,
+        "USD": 50000
+    }
+}, {
+    id: "2",
+    balances: {
+        "Google": 10,
+        "USD": 50000
+    }
+}];
 
+const bids: Order[] = [];
+const asks: Order[] = [];
+
+
+//place an order
+app.post('/order',async(req:any,res:any)=>{
+    const side : string = req.body.side;
+    const price: number = req.body.price;
+    const quantity : number = req.body.quantity;
+    const userId : string = req.body.userId;
+
+    const remainingQty = fillOrders(side, price, quantity,userId);
+
+    if(remainingQty === 0){
+        res.json({filledQuantity: quantity});
+        return;
+    }
+    if(side === "bid"){
+        bids.push({
+            userId,
+            price,
+            quantity:  remainingQty,
+        });
+        bids.sort((a,b)=> a.price < b.price ? -1 : 1);
+    }else{
+        asks.push({
+            userId,
+            price,
+            quantity: remainingQty
+        })
+        asks.sort((a,b)=> a.price < b.price ? 1 : -1);
+    }
+    res.json({
+        filledQuantity : quantity -  remainingQty
+    })
+})
+
+
+
+
+
+function fillOrders(side: string, price: number, quantity:number, userId: string):number{
+ return price;
+}
